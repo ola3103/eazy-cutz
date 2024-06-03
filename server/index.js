80a6d8b77b12f07357900e170c5e28060c41cf9a
ola3103 <wonuola.og31@yahoo.com>
2024-06-03 15:25:45 +0100

    vercel bug fixed 019

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? `https://eazy-cut.vercel.app`
+        ? `https://eazy-cutz.vercel.app`
         : `http://localhost:5173`,
     credentials: true,
   })
